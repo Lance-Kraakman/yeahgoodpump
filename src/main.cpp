65cf140b3f71b8ebb3696a1abf29b316dacc4a14
Lance Kraakman <Lance.Kraakman@eroad.com>
2019-12-03 16:42:13 +1300

    added a test http service

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <HTTPClient.h>
 #include "WiFi.h"
 #include "time.h"
 
@@ -8,8 +9,6 @@ const char* password   = "FVBLEW5LVT";
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
-
-
 
 void printLocalTime()
 {
@@ -32,8 +31,9 @@ int getSeconds() {
 
 void setup()
 {
+  //Set maximum frequency -Optimize peformance (Power is not an issue)
+  setCpuFrequencyMhz(240); 
   Serial.begin(9600);
-  
   pinMode(GPIO_NUM_16, OUTPUT);
 
   //connect to WiFi
@@ -55,18 +55,45 @@ void setup()
   WiFi.mode(WIFI_OFF);
 }
 
+
+//this wont work we need to schedule the tasks. with a task scheduler
+//most likely freeRTOS. 
+
 void loop()
 {
  
   delay(1000);
 
+  if (WiFi.status()==WL_CONNECTED) {
+    HTTPClient http;
+  //in the future this will be our own url -> for dev localhost:8080 etc (make springboot application using spring inititilizer)
+  //which does the ui and back end etc and then we can get data from that. 
+    http.begin("http://jsonplaceholder.typicode.com/comments?id=10"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if (httpCode > 0) { //Check for the returning code
+ 
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
   if ((getSeconds() == 20)|(getSeconds() == 40)) {
     Serial.println("We made it baby");
     digitalWrite(GPIO_NUM_16, HIGH);
     delay(5000);
     digitalWrite(GPIO_NUM_16, LOW);
         
-  }
+  } 
   Serial.println(getSeconds());  
+
+  
+
 
 }
